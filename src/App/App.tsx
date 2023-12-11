a5bca5791223da0967669aef10205968d6f9084c
Iaroslav Kukharenko <113698528+yar1kukharenko@users.noreply.github.com>
2023-12-12 02:23:47 +0300

    Add Pusher

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,9 @@
 import '../App.css';
-import { Route, Routes } from 'react-router-dom';
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
+import { useEffect, useRef } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 import Footer from 'components/Footer';
 import Header from 'components/Header';
 import ArticleListPage from './pages/ArticleListPage';
@@ -8,10 +12,48 @@ import HomePage from './pages/HomePage';
 import Login from './pages/Login';
 import MePage from './pages/MePage/MePage.tsx';
 import Registration from './pages/Registration';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 function App() {
+  const echoInitialized = useRef(false);
+
+  useEffect(() => {
+    if (!echoInitialized.current) {
+      window.Pusher = Pusher;
+      window.Echo = new Echo({
+        broadcaster: 'pusher',
+        key: 'a0690b63d24efe7a4744',
+        cluster: 'eu',
+        encrypted: true,
+        // Добавьте следующую строку для работы с авторизированными каналами
+        auth: {
+          headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('access_token'),
+          },
+        },
+      });
+      const token = localStorage.getItem('access_token');
+      if (token) {
+        window.Echo.channel(`articles`).listen('ArticleCreated', (e) => {
+          toast(<Link to={`http://localhost:8000/article/${e.articleId}`}>Новая статья!</Link>);
+        });
+      }
+      echoInitialized.current = true;
+    }
+  }, []);
   return (
     <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,8 @@ import { setupStore } from './store/store.ts';
 
 const store = setupStore();
 
+// Инициализация Echo с Pusher
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
